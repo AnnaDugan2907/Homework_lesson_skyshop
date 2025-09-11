27c30ec5410fe476a9e559abae7fdf7ccc95274d
AnnaDugan2907 <annadugan32@gmail.com>
2025-09-12 02:24:37 +0500

    Added homework 4

--- src/org/skypro/skyshop/searchEngine/SearchEngine.java
+++ src/org/skypro/skyshop/searchEngine/SearchEngine.java
@@ -8,7 +8,7 @@ public class SearchEngine {
     private static Searchable[] searchables = new Searchable[MAX_SIZE];
     private int size;
 
-    public Searchable[] search(String searchString) {
+    public Searchable[] searchs(String searchString)  {
 
         Searchable[] results = new Searchable[5];
         int count = 0;
@@ -26,7 +26,87 @@ public class SearchEngine {
         return results;
     }
 
-     public void add(Searchable items) {
+    public Searchable search(String search) throws BestResultNotFound {
+        Searchable[] results = new Searchable[5];
+        int count = 0;
+
+        if (search == null || search.isEmpty()) {
+            throw new BestResultNotFound(search);
+        }
+
+        Searchable bestMatch = null;
+        int maxCount = -1;
+
+        String searchLower = search.toLowerCase();
+
+        for (int i = 0; i < size; i++) {
+            Searchable item = searchables[i];
+            if (item != null) {
+                String searchTerm = item.getSearchTerm().toLowerCase();
+
+                // Подсчёт количества вхождений
+                int occurrences = countSubstringOccurrences(searchTerm, searchLower);
+                results[count] = item;
+
+                if (occurrences > maxCount) {
+                    maxCount = occurrences;
+                    bestMatch = item;
+                }
+
+                count++;
+            }
+        }
+
+        if (maxCount <= 0) {
+            throw new BestResultNotFound(search);
+        }
+        return bestMatch;
+    }
+
+
+//    public Searchable[] search(String search) throws BestResultNotFound {
+//        Searchable[] results = new Searchable[5];
+//        int count = 0;
+//
+//        if (search == null || search.isEmpty()) {
+//            throw new BestResultNotFound(search);
+//        }
+//
+//        String searchLower = search.toLowerCase();
+//
+//        for (int i = 0; i < size; i++) {
+//            Searchable item = searchables[i];
+//            if (item != null) {
+//                String searchTerm = item.getSearchTerm().toLowerCase();
+//                int occurrences = countSubstringOccurrences(searchTerm, searchLower);
+//                if (occurrences > 0) {
+//                    results[count] = item;
+//                    count++;
+//                }
+//            }
+//        }
+//
+//        if (count == 0) {
+//            throw new BestResultNotFound(search);
+//        }
+//
+//        // Возвращаем только заполненную часть массива
+//        return Arrays.copyOf(results, count);
+//    }
+
+
+    private int countSubstringOccurrences(String str, String sub) {
+        int count = 0;
+        int fromIndex = 0;
+        while ((fromIndex = str.indexOf(sub, fromIndex)) != -1) {
+            count++;
+            fromIndex += sub.length();
+        }
+        return count;
+    }
+
+
+    public void add(Searchable items) {
         if (size >= searchables.length) {
             System.out.println("Массив полон, добавление невозможно");
             return;

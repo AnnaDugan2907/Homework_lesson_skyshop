da2c79cc06b39a1b83f541f6eef8d64626ad29e3
AnnaDugan2907 <annadugan32@gmail.com>
2025-10-05 02:43:43 +0500

    Added homework 7

--- src/org/skypro/skyshop/article/Article.java
+++ src/org/skypro/skyshop/article/Article.java
@@ -45,13 +45,13 @@ public final class Article implements Searchable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Article article = (Article) o;
-        return Objects.equals(title, article.title) && Objects.equals(text, article.text);
+        return Objects.equals(title, article.title);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(title, text);
+        return Objects.hash(title);
     }
 
 }
